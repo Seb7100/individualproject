4797538c247b66e21455a6f4959dcaead5a097a2
Seb7100 <s.e.b7100@hotmail.com>
2020-12-08 00:46:55 +0100

    menu tilføjet, comments, header og farve ændringer

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,22 +1,23 @@
-/* const knappizza = getElementById('knap1-1');
-const knapsalat = getElementById('knap2-1');
-const knapbof = getElementById('knap3-1');
-
-const pizza = getElementByClassName('.item-wrapper1');
-const salat = getElementByClassName('.item-wrapper2');
-const bof = getElementByClassName('.item-wrapper3');
-
-function sePizza () {
-    if (pizza.style.display === "block") {
-
+$(document).ready(function () {
+  //Load når dokumentet er klar, for at forhindre jquery kører inden dokumentet er klar, det kan være f.eks. hvis den vil prøve at gemme et element som ikke er der endnu, så er det en god ide at først køre jquery når det er loadet
+  $(".btn1").click(function () {
+    //Klik på knap der ligger i menu-skift i html
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      //Hvis værdien står på 'all' i data-filter i html --->
+      $(".item").show("1000"); //Her viser vi alle items med 1000ms i hastighed som defineres i parentesen .show, der kan også bruges "slow" og "fast".
+    } else {
+      $(".item") //Definer element
+        .not("." + value) //Her definerer vi, at den ikke skal røre datafilter som er i value
+        .hide("1000"); // magen til display:none i css
+      $(".item")
+        .filter("." + value)
+        .show("1000");
     }
-} */
 
-var knapper = document.querySelectorAll(".menu-skift");
-
-knapper.forEach((knap) => {
-    knap.addEventListener("click", ()=> {
-        console.log("clicked");
-    })
+    //Slet aktiv class på det trykkede element
+    $(".btn1").click(function () {
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+  });
 });
-
